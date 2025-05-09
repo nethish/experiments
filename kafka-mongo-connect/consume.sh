21444d47ee9f44d1195999c0e02673b6310e36f1
Nethish <nethish259@gmail.com>
2025-05-09 14:16:24 +0530

    chore: fix kafka topic

--- kafka-mongo-connect/consume.sh
+++ kafka-mongo-connect/consume.sh
@@ -1,4 +1,3 @@
 # Run Kafka console consumer
 docker exec -it $(docker ps -qf "ancestor=confluentinc/cp-kafka:7.5.0") \
   kafka-console-consumer --bootstrap-server localhost:9092 --topic mongo.testdb.testcoll --from-beginning
-
--- kafka-mongo-connect/register-mongo-source.sh
+++ kafka-mongo-connect/register-mongo-source.sh
@@ -3,11 +3,10 @@ curl -X POST http://localhost:8083/connectors -H "Content-Type: application/json
   "config": {
     "connector.class": "com.mongodb.kafka.connect.MongoSourceConnector",
     "tasks.max": "1",
-    "connection.uri": "mongodb://mongo:27017",
+    "connection.uri": "mongodb://mongo1:27017,mongo2:27017,mongo3:27017/?replicaSet=myReplicaSet",
     "database": "testdb",
     "collection": "testcoll",
-    "topic.prefix": "mongo.",
+    "topic.prefix": "mongo",
     "output.format.value": "json"
   }
 }'
-
