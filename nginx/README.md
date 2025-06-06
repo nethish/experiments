abfa1e1fb005167875e132cdff2df4dcd71b523c
Nethish <nethish259@gmail.com>
2025-06-07 01:01:31 +0530

    feat: nginx load balancing

--- nginx/README.md
+++ nginx/README.md
@@ -1,2 +1,5 @@
 # NGINX
 A single proxy server that handles all the incoming connections, and forwards it to backend servers based on different paths
+
+## Load Balancing
+The upstream section defines list of servers for which the traffic will be routed in round robin fashion
--- nginx/backend/server.py
+++ nginx/backend/server.py
@@ -1,9 +1,15 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
+import os
+
 
 class SimpleHandler(BaseHTTPRequestHandler):
     def do_GET(self):
+        name = os.getenv("app_name")
         self.send_response(200)
         self.end_headers()
-        self.wfile.write(b"Hello from Backend API")
+        message = "Hello from Backend server: " + name
+        print(message)
+        self.wfile.write(bytes(message, encoding="utf-8"))
 
-HTTPServer(('0.0.0.0', 5000), SimpleHandler).serve_forever()
+
+HTTPServer(("0.0.0.0", 5000), SimpleHandler).serve_forever()
