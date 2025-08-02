941700a4041d1fedd0a2ac642e56def8abe72cfe
Nethish Rajendran <nethish259@gmail.com>
2025-08-02 22:11:07 +0530

    feat: add mongo repl set example

---
+++ mongo-replset/init.sh
@@ -0,0 +1,9 @@
+docker exec -it mongo1 mongosh --eval 'rs.initiate({                                  rbenv:2.7
+  _id: "rs0",
+  members: [
+    { _id: 0, host: "mongo1:27017" },
+    { _id: 1, host: "mongo2:27017" },
+    { _id: 2, host: "mongo3:27017" }
+  ]
+})
+'
